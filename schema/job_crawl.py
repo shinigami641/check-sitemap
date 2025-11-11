c2ea12145498a7573fd48454968ab890809ca85a
M Anang R <93875564+shinigami641@users.noreply.github.com>
2025-11-11 22:26:05 +0700

    feat: schema tbl job crawl

---
+++ schema/job_crawl.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from config.mysql import Base
+from sqlalchemy import DateTime
+from datetime import datetime
+
+class JobCrawl(Base):
+    __tablename__ = "jobs_crawl"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    job_id = Column(String(150), unique=True, index=True)
+    url = Column(String(150), nullable=False, index=True)
+    created_at = Column(DateTime, default=datetime.now)
+    
+    def __repr__(self):
+        return f"JobCrawl(id={self.id}, job_id={self.job_id}, url={self.url}, created_at={self.created_at})"
