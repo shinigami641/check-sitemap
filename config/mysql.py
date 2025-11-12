c2c01cf127f9d78457c38649a9ae48cff39ce00d
M Anang R <93875564+shinigami641@users.noreply.github.com>
2025-11-12 10:47:42 +0700

    feat: debug mysql false

--- config/mysql.py
+++ config/mysql.py
@@ -9,7 +9,7 @@ DB_NAME = "project_sitemap"
 
 database_url = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(database_url, pool_size=10, max_overflow=20, echo=True)
+engine = create_engine(database_url, pool_size=10, max_overflow=20)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base = declarative_base()
